aa1c647567e4551fa64b8c57451925295aa7b54b
GonzaBariloche <121523940+GonzaBariloche@users.noreply.github.com>
2024-02-06 13:44:43 +0000

    2da carga

--- wp-config.php
+++ wp-config.php
@@ -20,17 +20,17 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define('DB_NAME', 'bgrs12t9zjat4gvfuuhx');
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define('DB_USER', 'upbergooiue2xocr');
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define('DB_PASSWORD', 'V79LWSupZiaoLHr11KHk');
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
-
+define('DB_HOST', 'bgrs12t9zjat4gvfuuhx-mysql.services.clever-cloud.com');
+define('DB_PORT', '3306');
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
 
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         '>>&1bBx#-xNW-!NMQLz~j0|rZ{Hm6c1x|NwnpP{aAgj$<{4z,Z|s/52`{PD]B@4#');
+define('SECURE_AUTH_KEY',  '^]s3,dNa&)r-d.p}W|xUYJP_{%D[>@.7}@E5|TdgP8hh^V8#]&,#s7zqw.*4tgx{');
+define('LOGGED_IN_KEY',    '~aF5cuOTFtsi4^7AZesrd={d[dW,{k&SY:[-%498hfN2#haO| .}`9jZA=Uh-FN/');
+define('NONCE_KEY',        ';}I0WHQ&,n/=t]Z+qx{)r:&&$X$`!nw|><&iY Ar!}:]C<,|eLA8^d]Mk`e:?Trm');
+define('AUTH_SALT',        'T/St)-:e:.@-o>p aioj8> RAly:4-2#tC?N(CcgG)z& :gfiH[-W&.>GNcL0dN}');
+define('SECURE_AUTH_SALT', 'K.oCWG}reJaK#==~Lk3LcP[E<5eHuF+i}~{(8q023+-p|cM+7{*lb{:md|>oB%lf');
+define('LOGGED_IN_SALT',   'aeikD;y%xH?+$cI{i&io:}[BcXBY9sOe*j-*F/+?PK[}FgLYcL8BOo8]xx{->u_J');
+define('NONCE_SALT',       '5;B9!9:*Z`vzP7k.|9d`{|WrcTN+lI3L]O=b=[-o5;`T:+9bvprWe(LK[tTcF6c*');
 
 /**#@-*/
 
@@ -95,6 +95,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
 
-/**INICIO DE EDICIÓN */
-nano wordpress/wp-config.php
 
