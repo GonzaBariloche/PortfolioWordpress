1f5c4ddece059b47d09fdf98b4f6f486e9580e51
GonzaBariloche <121523940+GonzaBariloche@users.noreply.github.com>
2024-02-06 13:20:05 +0000

    hola

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PortfolioWordpress
-Portfolio diseñado en Wordpress
+# Portfolio diseñado en Wordpress
+# Iniciado el 06/02/2024 de manera virtual con GitHub CodeSpaces
---
+++ wordpress/wp-config.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * ... (comentarios y definiciones de configuración)
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+define( 'DB_NAME', 'database_name_here' );
+define( 'DB_USER', 'username_here' );
+define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_HOST', 'localhost' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ * ... (definiciones de claves y sales)
+ */
+$table_prefix = 'wp_';
+
+define( 'WP_DEBUG', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
+
+
